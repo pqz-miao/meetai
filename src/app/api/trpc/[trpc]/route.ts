af2180bfa9e81deca93476de31a56d7efaa8d23c
miao-pqz <1125583676.qq.com>
2025-06-27 11:32:17 +0800

    08: trpc setup fix

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-import { createTRCPContext } from "@/trpc/init";
+import { createTRPCContext } from "@/trpc/init";
 import { appRouter } from "@/trpc/routers/_app";
 
 const handler = (req: Request) => 
@@ -8,7 +8,7 @@ const handler = (req: Request) =>
         endpoint: "/api/trpc",
         req,
         router: appRouter,
-        createContext: createTRCPContext,
+        createContext: createTRPCContext,
     });
 
 export { handler as GET, handler as POST };
