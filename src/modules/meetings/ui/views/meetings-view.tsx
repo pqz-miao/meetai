0954839b3ae661dad84c5e15c581b8482fb40089
miao-pqz <1125583676.qq.com>
2025-07-01 09:23:18 +0800

    16: meetings setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+
+import { useTRPC } from "@/trpc/client";
+
+export const MeetingsView = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+    return (
+        <div>
+            {JSON.stringify(data, null, 2)}
+        </div>
+    );
+};
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState
+            title="Loading Meetings"
+            description="This may take a few seconds"
+        />
+    );
+};
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState
+            title="Error Loading Meetings"
+            description="Something went wrong"
+        />
+    );
+};
